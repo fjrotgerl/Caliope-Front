14178c78f3046b3cc2cdd113883c5aad3e350f6a
javier rotger <fjrotgerl@gmail.com>
2019-05-25 17:18:48 +0200

    home "finalizado"

--- src/pages/public/Index.vue
+++ src/pages/public/Index.vue
@@ -1,5 +1,5 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-<q-page class="bg-img">
+<q-page class="">
 <!--    <q-knob-->
 <!--      show-value-->
 <!--      class="text-white q-ma-md"-->
@@ -12,8 +12,10 @@
 <!--    ><q-icon name="volume_up" />-->
 <!--    </q-knob>-->
 <!--      <q-carousel-slide :name="1" img-src="../statics/carousel/festival1.jpg" />-->
-  <div class="q-pa-md">
-
+  <div class="absolute-center text-white q-pa-md">
+    <h1 class="mb-less-5 text-align-center text-weight-bold-450">CALÍOPE</h1>
+    <h3 class="mb-less-5 ">Crea y comparte tu música con el mundo.</h3>
+    <h3>Tus creaciones no tienen límites.</h3>
   </div>
   </q-page>
 </template>
