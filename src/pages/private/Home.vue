aa2d09ae3b44233a117049a5b24ffc8884935d57
javier rotger <fjrotgerl@gmail.com>
2019-05-25 19:28:58 +0200

    ya se puede modificar el volumen de la cancion actual

--- src/pages/private/Home.vue
+++ src/pages/private/Home.vue
@@ -1,12 +1,28 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <q-page class="flex column">
 
+
+    <q-knob
+      show-value
+      class="text-white q-ma-md"
+      v-model="songVolume"
+      size="60px"
+      @drag-value="test"
+      :thickness="0.2"
+      color="orange"
+      center-color="black"
+      track-color="transparent"
+    ><q-icon name="volume_up" />
+    </q-knob>
+
+
     <div class="row justify-around" style="padding: 0 20px;padding-bottom:20px;">
       <div class="col-9">
         <h2>Feed</h2>
         <div class="flex column justify-between">
           <div class="row flex cancion">
-          <q-btn icon="play_arrow" color="primary" style="margin-right: 20px;"></q-btn>
+          <q-btn @click="toogleSong" :icon="isSongPlaying ? 'pause' : 'play_arrow'" color="primary" style="margin-right: 20px;"></q-btn>
+            <q-btn @click="stopSong" icon="stop" color="primary" style="margin-right: 20px;"></q-btn>
           <div class="flex column justify-between" style="width:90%;">
             <div class="flex row justify-around">
               <span>Nombre Canción</span>
@@ -135,7 +151,31 @@
 </style>
 
 <script>
+  import audioPlayer from '../../statics/js/audioPlayer'
+  import constants from '../../statics/js/configuration'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      isSongPlaying: false,
+      songVolume: constants.DEFAULT_SONG_VOLUME,
+
+      toogleSong: () => {
+        audioPlayer.toogle();
+        this.isSongPlaying = audioPlayer.getSongStatus();
+      },
+      stopSong: () => {
+        audioPlayer.stop();
+      },
+      test: () => {
+        if (this.songVolume < 10) {
+          audioPlayer.changeVolume("0.0" + this.songVolume);
+        } else {
+          audioPlayer.changeVolume("0." + this.songVolume);
+        }
+      }
+    }
+  }
 }
 </script>
--- src/statics/js/configuration.js
+++ src/statics/js/configuration.js
@@ -1,4 +1,5 @@
 exports.AUTH_API_URL = "http://localhost:3002";
-
 exports.REST_API_URL = "http://localhost:8080";
+
+exports.DEFAULT_SONG_VOLUME = 50;
 
